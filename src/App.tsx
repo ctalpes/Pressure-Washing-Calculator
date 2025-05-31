ac88bc112d76381e183c80a39e2a005d05ce2bb7
ctalpes <Talpes.christian@yahoo.com>
2025-05-30 23:24:04 -0400

    Restore App.tsx with basic template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Pressure Washing Calculator</h1>
+      <p>Welcome! Start building your calculator here.</p>
+    </div>
+  );
+};
+
+export default App;
